3ab6eff591713b9a514644bf3c040a196f209f06
jdheeter <johndheeter@gmail.com>
2024-10-13 20:44:28 -0400

    add tos page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,7 +8,8 @@ const routes:RouteRecordRaw[] = [
       { name: "index", path: "", component: () => import("pages/IndexPage.vue") },
       { name: "upload", path: "/upload", component: () => import("pages/UploadPage.vue") },
       { name: "prompt", path: "/promptTranscription/:id?", component: () => import("pages/PromptPage.vue") },
-      { name: "live", path: "/live/:id?", component: () => import("pages/LivePage.vue") }
+      { name: "live", path: "/live/:id?", component: () => import("pages/LivePage.vue") },
+      { name: "tos", path: "/tos", component: () => import("pages/Tos.vue") }
     ]
   },
 
