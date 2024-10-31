337c695b9edd28e3aa582021ab55e24996d644b4
Boid <john@boid.com>
2024-10-31 12:46:23 -0400

    testing

--- netlify/functions/renderMetadata.ts
+++ netlify/functions/renderMetadata.ts
@@ -17,14 +17,10 @@ const generateMetadata = async (dynamicId: string, index: string): Promise<Metad
 
 const handler: Handler = builder(async (event) => {
   const { path, queryStringParameters, headers } = event
-  const queryParamsString = queryStringParameters.toString()
   const dynamicId = path?.split("/").pop() || ""
   const index = queryStringParameters?.index || "1"
 
   const pageData = await generateMetadata(dynamicId, index)
-
-  const userAgent = headers["user-agent"] || ""
-  const isBot = /bot|crawl|spider|slurp|facebookexternalhit|linkedinbot|embedly|quora|pinterest|reddit|slackbot|twitterbot|whatsapp/i.test(userAgent)
 
   return {
     statusCode: 200,
@@ -40,28 +36,20 @@ const handler: Handler = builder(async (event) => {
 
       <!-- Open Graph / Facebook -->
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://alpha.fiddl.art/r/StxXwHQySwGBzFrsGFJKIQ?index=0&referredBy=JD" />
+      <meta property="og:url" content="https://alpha.fiddl.art/r/StxXwHQySwGBzFrsGFJKIQ?index=0" />
       <meta property="og:title" content="Page for StxXwHQySwGBzFrsGFJKIQ - Index 1" />
       <meta property="og:description" content="This is a description for StxXwHQySwGBzFrsGFJKIQ at index 1." />
       <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
 
       <!-- Twitter -->
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://alpha.fiddl.art/r/StxXwHQySwGBzFrsGFJKIQ?index=0&referredBy=JD" />
+      <meta property="twitter:url" content="https://alpha.fiddl.art/r/StxXwHQySwGBzFrsGFJKIQ?index=0" />
       <meta property="twitter:title" content="Page for StxXwHQySwGBzFrsGFJKIQ - Index 1" />
       <meta property="twitter:description" content="This is a description for StxXwHQySwGBzFrsGFJKIQ at index 1." />
       <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
 
       <!-- Meta Tags Generated with https://metatags.io -->
-        ${
-          isBot
-            ? ""
-            : `
-          <script>
-            window.location.replace("https://alpha.fiddl.art${path}?index=${index}");
-          </script>
-        `
-        }
+
       </head>
       <body>
         <h1>${pageData.title}</h1>
