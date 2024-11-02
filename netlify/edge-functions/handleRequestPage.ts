84b2a06684d5951c73d1ade488001d888d1be479
Boid <john@boid.com>
2024-11-02 01:37:59 -0400

    seems to work

--- netlify/edge-functions/handleRequestPage.ts
+++ netlify/edge-functions/handleRequestPage.ts
@@ -12,7 +12,15 @@ export default async (request: Request, context: Context) => {
 
   const segments = url.pathname.split("/")
   const id = segments[2]
-  const imageUrl = `https://api.fiddl.art/images/${shortIdToLong(id)}-md.webp`
+  console.log(id)
+  const longId = shortIdToLong(id)
+  const requestData = await fetch(`https://api.fiddl.art/trpc/creations.createRequest?input="${encodeURIComponent(longId)}"`)
+  // const requestData = await fetch(`http://localhost:4444/trpc/creations.createRequest?input="${encodeURIComponent(longId)}"`)
+  const jsonData = await requestData.json()
+  const images = jsonData.result.data.imageIds
+  const imageId = images[url.searchParams.get("index") || 0]
+  const imageUrl = `https://api.fiddl.art/images/${imageId}-md.webp`
+  // const imageUrl = `http://localhost:4444/images/${imageId}-md.webp`
   const updatedHtml = setSocialMetadata(text, id || "image id", "A creation on Fiddl.art", imageUrl, url.toString(), "summary_large_image")
   return new Response(updatedHtml, response)
 }
