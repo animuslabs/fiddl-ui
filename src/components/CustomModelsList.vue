080d6cfaebb5b8fa1670c40c65c50c2328442d34
Boid <john@boid.com>
2024-11-18 21:53:24 -0500

    add the ability to edit the name of custom models in the models list

--- src/components/CustomModelsList.vue
+++ src/components/CustomModelsList.vue
@@ -2,8 +2,11 @@
   QList(v-if="models?.length").full-width
     QItem(v-for="model in models" :key="model.id")
       .row.q-gutter-md.items-center
-        div
-          q-btn(icon="delete" flat color="red" @click="deleteModel(model)")
+        .col-auto
+          .row
+            q-btn(icon="delete" flat color="accent" @click="deleteModel(model)")
+          .row
+            q-btn(icon="edit" flat color="white" @click="editModelName(model)")
         div.cursor-pointer(@click="handleModelClick(model)")
           .q-ml-md(style="text-transform: capitalize;")
             h3 {{ model.name }}
@@ -24,14 +27,23 @@
               .row.q-ml-lg
                 div(v-for="imageRequest in model.imageRequests" :key="imageRequest.id")
                   .relative-position(style="width:60px; height:50px;")
-                    img(:src="img((imageRequest.images[0]?.id||''),'sm')" style="max-width:100px; max-height:100px; object-fit:contain; margin:5px; border-radius:25px; position:absolute; bottom:-30px; box-shadow: 0px 1px 15px rgba(1,1,1,.1) !important")
+                    img(:src="img((imageRequest.images[0]?.id||''),'md')" style="width:100px; height:100px; object-fit:cover; margin:5px; border-radius:25px; position:absolute; bottom:-30px; box-shadow: 0px 1px 15px rgba(1,1,1,.1) !important")
         .full-width
           q-separator(color="grey" spaced="20px")
+
   div(v-else)
     .full-width(style="height: '100px'")
       .centered
         h6 No custom models, create one to get started
-
+  q-dialog(ref="editModelDialog" v-model="showEditNameDialog")
+    q-card
+      .q-ma-md
+        h5.q-mb-sm Edit Model Name
+        q-input.q-mb-lg(v-model="newModelName" )
+        .row.q-gutter-md
+          q-btn(label="Cancel" @click="showEditNameDialog=false" color="accent" flat)
+          .col-grow
+          q-btn(label="Update" @click="setModelName()" color="primary" flat)
 </template>
 <script lang="ts">
 import { CustomModel } from "lib/api"
@@ -51,6 +63,9 @@ export default defineComponent({
       models: [] as CustomModel[] | null,
       timeSince,
       img,
+      newModelName: "",
+      showEditNameDialog: false,
+      editingModelNameId: null as string | null,
     }
   },
   watch: {
@@ -66,6 +81,18 @@ export default defineComponent({
     console.log("mounted")
   },
   methods: {
+    async setModelName() {
+      if (!this.editingModelNameId) return
+      await this.$api.models.setModelName.mutate({ id: this.editingModelNameId, name: this.newModelName }).catch(catchErr)
+      this.showEditNameDialog = false
+      Notify.create("Model name updated")
+      await this.loadData()
+    },
+    editModelName(model: CustomModel) {
+      this.editingModelNameId = model.id
+      this.newModelName = model.name
+      this.showEditNameDialog = true
+    },
     async loadData() {
       console.log("loadData")
       this.models = (await this.$api.models.getUserModels.query().catch(catchErr)) || null
