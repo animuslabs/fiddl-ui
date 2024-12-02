dfe205749063c735d7dadfdbbef8844f8ddf4aac
Boid <john@boid.com>
2024-12-01 21:31:46 -0500

    increase size of image cache

--- src/lib/hdImageCache.ts
+++ src/lib/hdImageCache.ts
@@ -2,7 +2,7 @@ import { openDB, IDBPDatabase } from "idb"
 
 const CACHE_DB_NAME = "ImageCacheDB"
 const CACHE_STORE_NAME = "ImageStore"
-const MAX_CACHE_SIZE = 3 * 1024 * 1024 // 3 MB limit
+const MAX_CACHE_SIZE = 30 * 1024 * 1024
 
 /**
  * Open or initialize the IndexedDB.
