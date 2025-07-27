c7e27d9167f064a9489eff23182f20c35c29e6c7
jdheeter <jdheeter@gmail.com>
2025-07-27 22:04:29 +0700

    add icons to model cards

--- src/components/ModelCard.vue
+++ src/components/ModelCard.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
-q-card.q-pa-md(style="position: relative; overflow: hidden;" @click.stop="console.log('hi')" :style="selectable ? 'cursor:pointer' : ''")
+q-card.q-pa-md(
+  style="position: relative; overflow: hidden;"
+  @click.stop="console.log('hi')"
+  :style="mergedCardStyle"
+).relative-position
   .card-bg(:style="cardBgStyle" style="height:250px; position: absolute;")
   .row(style="position: relative; z-index: 1;")
     //- //- .col-auto(style="width:40px;")
@@ -11,8 +15,10 @@ q-card.q-pa-md(style="position: relative; overflow: hidden;" @click.stop="consol
       .row.ellipsis-2-lines(style="height:50px;")
         p {{ model.description }}
       .row.q-mt-md
-        q-chip(v-for="tag in model.modelTags" :key="tag" color="grey-9" text-color="white" clickable @click.stop="$emit('chipClick', tag)")
+        q-chip(v-for="tag in model.modelTags" :key="tag" text-color="white" clickable @click.stop="$emit('chipClick', tag)")
           small {{ tag }}
+    div.q-pa-md(style="right:0px; bottom:-20px; position: absolute;")
+      q-icon(:name="isVideo?'smart_display':'image'" size="35px"  :style="{ color: 'white', opacity: 0.5 }")
 </template>
 
 <script setup lang="ts">
@@ -32,6 +38,8 @@ defineEmits({
   chipClick: (tag: ModelTags) => true,
 })
 
+const isVideo = computed(() => props.model.modelTags.includes("Video"))
+
 const cardBgStyle = computed(() => {
   const { modelTags, previewMediaId } = props.model
   let mediaUrl = modelTags.includes("Video") ? s3Video(previewMediaId || "", "thumbnail") : img(previewMediaId || "", "md")
@@ -42,6 +50,30 @@ const cardBgStyle = computed(() => {
     // position: "absolute",
     inset: "0",
     zIndex: 0,
+  }
+})
+
+const mergedCardStyle = computed(() => {
+  const base: any = cardBorder.value
+  if (props.selectable) {
+    base.cursor = "pointer"
+  }
+  return base
+})
+
+const cardBorder = computed(() => {
+  const { modelTags } = props.model
+  const isVideo = modelTags.includes("Video")
+  // Use Quasar theme CSS vars
+  const color = isVideo ? "var($q-indigo)" : "var(--q-primary, #00adc7)"
+  // More visible, layered shadow for dark backgrounds
+  return {
+    // border: `0.5px solid ${color}`,
+    // boxShadow: `0 10px 20px ${color}`,
+    // shadow: `0 10px 20px ${color}`,
+    // borderRadius: "8px",
+    // backgroundColor: "rgba(0, 0, 0, 0.5)",
+    // color: "white",
   }
 })
 </script>
